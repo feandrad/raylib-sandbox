8e946058f3cb588e15dcd83b3c251b5943f98407
feandrad <feandrad@gmail.com>
2024-10-04 21:23:28 -0300

    cmake and tiled

---
+++ res/tiles/outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="outside" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="outside_tilemap.png" width="192" height="176"/>
+</tileset>
